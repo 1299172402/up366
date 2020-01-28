17009ff114134b763e1bd7323fef531162acf981
之雨商 <1299172402@qq.com>
2020-01-28 20:58:42 +0800

    获取直播课视频列表和地址

---
+++ getlist.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+from lxml import html
+import json
+# etree = html.etree
+
+# 欢迎文字
+print('\n\n')
+print('####### 天学网登录程序 #######\n')
+# print('Author~~')
+print('Jellow 看见我请一定一定叫我学习')
+print('Creative By ZhiyuShang With Love\n')
+# print('Thanks for being addicted')
+print('')
+
+
+print('测试获取直播课列表\n')
+print('出现我校直播课名称即为获取成功\n')
+
+
+headers = {
+	'Accept': 'application/json, text/javascript, */*; q=0.01',
+	'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+	'Host': 'live-api.up366.cn',
+	'Origin': 'http://me.up366.cn',
+	'Referer': 'http://me.up366.cn/center/student/course/liveclasses.html?courseId=89440&createTime=undefined',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+	'X-Requested-With': 'XMLHttpRequest',
+	'Cookie': 'acw_tc=7b39758215802092127684930e1234175f6acbea06d4bbf933eca866daecde; BIGipServercn_liveclass-api_pool=2467735744.38175.0000; SESSION=301df37e-80f0-4372-b3a1-2ab37363de0e'
+}
+
+body = {
+	'courseId': '89440',
+	'pager.currentPage': '18',
+	'pager.pageSize': '10',
+	'status': '0'
+}
+
+
+
+# 获取json
+play_url = 'http://live-api.up366.cn/client/liveclass/list'
+s = requests.session()
+response = s.post(play_url, headers=headers,data=body).content
+s = BeautifulSoup(response, 'lxml')
+s = s.find('p').text
+
+# 把json转化为python对象
+s = json.loads(s)
+
+# 简化数据
+course = s['data']['list']
+print(course)
+
+list1 = []
+for video in course:
+	vodUrl = 'http://fs.up366.cn/download/' + video['vodUrl']
+	list1.append([video['lcName'],video['updateTime'],vodUrl])
+
+print(list1)
+
+
+
+
+
+
+
+
+
+
+
+
